7eda323b4633c7751c2b821e600206b72f033958
Sergey Youmshanov <otto-vector@yandex.ru>
2019-10-12 11:40:10 +0400

    label to input & rem correct

--- src/templates/bem/elements/masked-text/masked-text.js
+++ src/templates/bem/elements/masked-text/masked-text.js
@@ -1,11 +1,13 @@
 import $ from 'jquery'
 import './utils/jquery.maskedinput.min.js';
 
-//маска для даты
+/* маска для даты */
+
 $(function() {
-	$.mask.definitions['D']='[0-3]';
-	$.mask.definitions['M']='[0-1]';
-	$.mask.definitions['Y']='[1-2]';
-	$.mask.definitions['y']='[0,9]';
-	$('.masked-text__input').mask("D9.M9.Yy99", { placeholder: "ДД.MM.ГГГГ" });
+  $.mask.definitions['D']='[0-3]';
+  $.mask.definitions['M']='[0-1]';
+//  $.mask.definitions['Y']='[1-2]';
+  $.mask.definitions['YY']='[19,20]';
+  $.mask.definitions['y']='[0,9]';
+  $('.masked-text__input').mask("D9.M9.YY99", { placeholder: "ДД.MM.ГГГГ" });
 });
