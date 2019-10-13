0313731937ad276489eb573151b30822341b624f
Sergey Youmshanov <otto-vector@yandex.ru>
2019-10-13 05:50:45 +0400

    some ref.changes.sup.standarts

--- src/templates/bem/elements/masked-text/masked-text.js
+++ src/templates/bem/elements/masked-text/masked-text.js
@@ -7,7 +7,7 @@ $(function() {
   $.mask.definitions['D']='[0-3]';
   $.mask.definitions['M']='[0-1]';
 //  $.mask.definitions['Y']='[1-2]';
-  $.mask.definitions['YY']='[19,20]';
+  $.mask.definitions['Y']='[19,20]';
   $.mask.definitions['y']='[0,9]';
-  $('.masked-text__input').mask("D9.M9.YY99", { placeholder: "ДД.MM.ГГГГ" });
+  $('.masked-text__input').mask("D9.M9.Yy99", { placeholder: "ДД.MM.ГГГГ" });
 });
