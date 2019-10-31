bd8ac4d2f211c9e05875c4d498ada95353a997cd
Sergey Youmshanov <otto-vector@yandex.ru>
2019-10-31 06:47:41 +0400

    add a toggle

--- src/templates/bem/blocks-library/blocks-library.js
+++ src/templates/bem/blocks-library/blocks-library.js
@@ -2,3 +2,4 @@ import '../input-field/input-field.js';
 import '../input-date-dropdown/input-date-dropdown.js';
 import '../logo/logo.js';
 import '../checkbox/checkbox.js';
+import '../toggle/toggle.js';
