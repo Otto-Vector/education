134fb23f0b7fec4f12b338f8631d12099e85b31b
Sergey Youmshanov <otto-vector@yandex.ru>
2019-11-05 03:09:06 +0400

    add like-button block

--- src/templates/bem/blocks-library/blocks-library.js
+++ src/templates/bem/blocks-library/blocks-library.js
@@ -3,3 +3,4 @@ import '../input-date-dropdown/input-date-dropdown.js';
 import '../logo/logo.js';
 import '../checkbox/checkbox.js';
 import '../toggle/toggle.js';
+import '../like-button/like-button.js';
